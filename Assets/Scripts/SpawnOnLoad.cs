ef6817be0cfa3e2bac22905786fcac781bfe92cb
patrickdevarney <patrickdevarney@gmail.com>
2020-01-07 10:53:43 -0600

    First commit

---
+++ Assets/Scripts/SpawnOnLoad.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class SpawnOnLoad : MonoBehaviour
+{
+    public AssetReference prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Addressables.InstantiateAsync(prefab);
+    }
+}
---
+++ Assets/Scripts/SpriteTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Rendering;
+using Unity.Transforms;
+
+public class SpriteTest : MonoBehaviour
+{
+    public Mesh mesh;
+    public Material material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EntityManager manager = World.Active.EntityManager;
+        Entity e = manager.CreateEntity(typeof(LocalToWorld), typeof(Translation));
+        manager.AddSharedComponentData<RenderMesh>(e, new RenderMesh
+        {
+            mesh = mesh,
+            material = material,
+        });
+    }
+}
