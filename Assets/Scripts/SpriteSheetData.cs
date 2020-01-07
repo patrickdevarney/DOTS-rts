aa6143cc1782efab812b26ac220dc5b3701da78e
patrickdevarney <patrickdevarney@gmail.com>
2020-01-07 12:41:36 -0600

    Setup sprite components and jobs

---
+++ Assets/Scripts/SpriteSheetData.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+
+public struct SpriteSheetData : IComponentData
+{
+    public int currentFrame;
+    public int totalFrameCount;
+    public float frameTimeRemaining;
+    public float maxFrameTime;
+}
+
+public class SpriteSheetAnimate : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        float deltaTime = Time.DeltaTime;
+        return Entities.ForEach((ref SpriteSheetData data) =>
+        {
+            data.frameTimeRemaining -= deltaTime;
+            if (data.frameTimeRemaining < 0f)
+            {
+                // proceed to next frame
+                data.frameTimeRemaining = data.maxFrameTime;
+                data.currentFrame = (data.currentFrame + 1) % data.totalFrameCount;
+            }
+            else
+            {
+                // stay on this frame
+            }
+        }).Schedule(inputDeps);
+    }
+}
+
+public class SpriteSheetRender : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref SpriteSheetData data, ref Translation translation) =>
+        {
+            MaterialPropertyBlock materialPropertyBlock = new MaterialPropertyBlock();
+            float uvWidth = 1f / 0.09375f;
+            float uvOffsetX = 0.09375f * data.currentFrame;
+            float uvHeight = 0.1875f;
+            float uvOffsetY = 0f;
+            Vector4 uv = new Vector4(uvWidth, uvHeight, uvOffsetX, uvOffsetY);
+            materialPropertyBlock.SetVectorArray("_MainTex_UV", new Vector4[] { uv });
+
+            Graphics.DrawMesh(SpriteTest.fooMesh, translation.Value, Quaternion.identity, SpriteTest.fooMaterial, 0, Camera.main, 0, materialPropertyBlock);
+        });
+    }
+}
--- Assets/Scripts/SpriteTest.cs
+++ Assets/Scripts/SpriteTest.cs
@@ -7,17 +7,27 @@ using Unity.Transforms;
 
 public class SpriteTest : MonoBehaviour
 {
+    public static Mesh fooMesh;
+    public static Material fooMaterial;
     public Mesh mesh;
     public Material material;
     // Start is called before the first frame update
     void Start()
     {
+        fooMesh = mesh;
+        fooMaterial = material;
         EntityManager manager = World.Active.EntityManager;
         Entity e = manager.CreateEntity(typeof(LocalToWorld), typeof(Translation));
-        manager.AddSharedComponentData<RenderMesh>(e, new RenderMesh
+        manager.AddComponentData(e, new SpriteSheetData
+        {
+            maxFrameTime = 0.5f,
+            totalFrameCount = 4,
+        }
+        );
+        /*manager.AddSharedComponentData(e, new RenderMesh
         {
             mesh = mesh,
             material = material,
-        });
+        });*/
     }
 }
